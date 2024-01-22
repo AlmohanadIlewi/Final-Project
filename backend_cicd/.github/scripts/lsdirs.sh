0b2a7c9acaf32d877a4389fd5c3b816142965ea4
Mohanad Ilewi <138300746+AlmohanadIlewi@users.noreply.github.com>
2024-01-22 11:44:46 +0100

    Delete backend_cicd directory

--- backend_cicd/.github/scripts/lsdirs.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-directories=$(find ./* -type d -prune)
-# echo "Directories found: $directories"
-dirs="["
-for dir in $directories; do
-    dirs="$dirs\"$(echo "$dir" | sed 's/.\///')\","
-done
-dirs="${dirs::-1}]"
-echo "$dirs"
