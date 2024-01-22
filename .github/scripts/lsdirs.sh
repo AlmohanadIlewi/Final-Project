6a757ebc86f52b5a3a38738ac861131a90091dfc
Al Mohanad Ilewi <mohanad.ilewi.83@gmail.com>
2024-01-22 14:43:15 +0100

    update the dates

---
+++ .github/scripts/lsdirs.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+directories=$(find ./* -type d -prune)
+# echo "Directories found: $directories"
+dirs="["
+for dir in $directories; do
+    dirs="$dirs\"$(echo "$dir" | sed 's/.\///')\","
+done
+dirs="${dirs::-1}]"
+echo "$dirs"
+
