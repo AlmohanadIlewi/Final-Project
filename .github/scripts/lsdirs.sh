da9691fba4287b6e22ba7aab799d0fdb2158a5b1
Al Mohanad Ilewi <mohanad.ilewi.83@gmail.com>
2024-01-25 12:37:57 +0100

    Backend Lambda Deploy S3

--- .github/scripts/lsdirs.sh
+++ .github/scripts/lsdirs.sh
@@ -1,4 +1,41 @@
 
+!/bin/bash
+directories=$(find ./* -type d -prune)
+dirs="["
+lambdi="{"
+count=0
+
+for dir in $directories; do
+    dirs="$dirs\"$(echo "$dir" | sed 's/.\///')\","
+    lambdi="$lambdi\"$count\": $(cat $dir/lambda_def.json), "
+    count=$((count+1))
+done
+
+dirs="${dirs::-1}]"
+lambdi="${lambdi::-2}}"
+
+echo "$lambdi" > lambdi.json
+echo "$dirs"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 # #!/bin/bash
 
 # # Step 1: List all directories in the current location
@@ -36,20 +73,3 @@
 # ohne Params, gibt es eine Liste von allen Verzeichnissen aus dem aktuellen Verzeichnis aus.
 # Erstellt ein JSON-Objekt mit allen Lambda-Definitionen
 
-
-directories=$(find ./* -type d -prune)
-dirs="["
-lambdi="{"
-count=0
-
-for dir in $directories; do
-    dirs="$dirs\"$(echo "$dir" | sed 's/.\///')\","
-    lambdi="$lambdi\"$count\": $(cat $dir/lambda_def.json), "
-    count=$((count+1))
-done
-
-dirs="${dirs::-1}]"
-lambdi="${lambdi::-2}}"
-
-echo "$lambdi" > lambdi.json
-echo "$dirs"
